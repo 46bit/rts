a3ae4b374d488d2ce2b2b96022f0b6e6cdd17c40
Michael Mokrysz <hi@46b.it>
2020-01-05 22:22:25 +0000

    Render projectiles as teardrop shapes
    It's much prettier than circles.

--- lib/entities/projectile.rb
+++ lib/entities/projectile.rb
@@ -5,8 +5,8 @@ class Projectile
   MAXIMUM_RANGE = 180
   COLLISION_RADIUS = 4
 
-  attr_reader :position, :direction, :velocity, :renderer
-  attr_reader :dead, :start_position, :circle, :damage_type
+  attr_reader :position, :direction, :velocity, :renderer, :damage_type
+  attr_reader :dead, :start_position, :circle, :triangle
 
   def initialize(position, direction, color, renderer, damage_type: :projectile_collision)
     @position = position
@@ -26,11 +26,26 @@ class Projectile
       opacity: 0.7,
       z: 5,
     )
+    vector_to_point_on_circle = vector_from_magnitude_and_direction(3, @direction + Math::PI / 2)
+    vector_to_tail_point = vector_from_magnitude_and_direction(6, @direction)
+    @triangle = @renderer.triangle(
+      x1: @position[0] + vector_to_point_on_circle[0],
+      y1: @position[1] + vector_to_point_on_circle[1] ,
+      x2: @position[0] - vector_to_point_on_circle[0],
+      y2: @position[1] - vector_to_point_on_circle[1],
+      x3: @position[0] - vector_to_tail_point[0],
+      y3: @position[1] - vector_to_tail_point[1],
+      color: color,
+      opacity: 0.7,
+      z: 5,
+    )
   end
 
   def kill
     @dead = true
-    @circle.remove unless HEADLESS
+    return if HEADLESS
+    @circle.remove
+    @triangle.remove
   end
 
   def update
@@ -47,5 +62,14 @@ class Projectile
 
     @circle.x = @position[0]
     @circle.y = @position[1]
+
+    vector_to_point_on_circle = vector_from_magnitude_and_direction(3, @direction + Math::PI / 2)
+    vector_to_tail_point = vector_from_magnitude_and_direction(6, @direction)
+    @triangle.x1 = @position[0] + vector_to_point_on_circle[0]
+    @triangle.y1 = @position[1] + vector_to_point_on_circle[1]
+    @triangle.x2 = @position[0] - vector_to_point_on_circle[0]
+    @triangle.y2 = @position[1] - vector_to_point_on_circle[1]
+    @triangle.x3 = @position[0] - vector_to_tail_point[0]
+    @triangle.y3 = @position[1] - vector_to_tail_point[1]
   end
 end
