87d33744eb276a004931b2662bfde79a88f96085
Michael Mokrysz <hi@46b.it>
2020-01-11 18:30:40 +0000

    Refactor Player class for ease of understanding
    The Player class does most of the game's logic: everything except
    collision detection and declaring a winner. I think it does way too
    much, but I'm not sure where to better put that logic. For now this
    commit refactors things to be easier to understand.

--- lib/player.rb
+++ lib/player.rb
@@ -42,61 +42,18 @@ class Player
   end
 
   def update(generators, other_players)
-    update_energy(generators)
+    remove_dead_units
 
-    powered_units = @factories.select(&:producing?)
-    powered_units += @vehicles.select { |u| u.respond_to?(:producing?) && u.producing? }
-    power_drains = Hash[powered_units.map do |powered_unit|
-      [powered_unit.object_id, powered_unit.energy_consumption]
-    end]
-    desired_energy_consumption = power_drains.values.sum
-    if desired_energy_consumption > @energy
-      power_per_unit = @energy / powered_units.length
-      power_drains.transform_values! { power_per_unit }
-    end
-    at_unit_cap =  unit_count < @unit_cap
-    powered_units.each do |powered_unit|
-      # FIXME: do something with unused_build_capacity
-      power_drain = power_drains[powered_unit.object_id]
-      powered_unit.energy_provided = power_drain
-      @energy -= power_drain
-    end
-
+    update_energy_generation(generators)
+    update_energy_consumption
     @factories.each(&:update)
     @vehicles.each(&:update)
-
-    @constructions.each do |construction|
-      if construction.built? && unit_count < @unit_cap
-        @constructions.delete(construction)
-        construction.prerender
-        case construction
-        when Factory
-          @factories << construction
-        when Turret
-          @turrets << construction
-        else
-          if construction.class.ancestors.include?(Vehicle)
-            @vehicles << construction
-          else
-            raise "unknown unit construction complete: '#{construction}'"
-          end
-        end
-      end
-    end
-
-    enemy_vehicles = (other_players.map(&:vehicles) + other_players.map(&:factories) + other_players.map(&:turrets)).flatten
-    @turrets.each do |turret|
-      projectile = turret.update(enemy_vehicles)
-      @projectiles << projectile unless projectile.nil?
-    end
-    @projectiles.each(&:update)
+    update_constructions
+    update_turrets_and_projectiles(other_players)
+    remove_dead_units
 
     @control.update(generators, self, other_players)
-
-    @vehicles.reject! { |v| v.dead }
-    @projectiles.reject! { |v| v.dead }
-    @turrets.reject! { |v| v.dead }
-    @constructions.reject! { |c| c.dead? }
+    remove_dead_units
   end
 
   def render
@@ -116,7 +73,7 @@ class Player
         )
       end
       pretty_build_capacity = @latest_build_capacity == @latest_build_capacity.to_i ? @latest_build_capacity.to_i : @latest_build_capacity
-      @stats_text.text = "#{unit_count}/#{@unit_cap} #{@energy.floor}+#{pretty_build_capacity}"
+      @stats_text.text = "#{unit_count}/#{@unit_cap} #{@energy.round}+#{pretty_build_capacity}"
       @stats_text.x = (oldest_factory.position[0] - 9.5)
       @stats_text.y = (oldest_factory.position[1] + 14)
     else
@@ -133,14 +90,67 @@ class Player
     @factories + @vehicles + @turrets + @constructions
   end
 
-  def update_energy(generators)
-    @latest_build_capacity = build_capacity(generators)
+protected
+
+  def remove_dead_units
+    @vehicles.reject! { |v| v.dead? }
+    @projectiles.reject! { |v| v.dead? }
+    @turrets.reject! { |v| v.dead? }
+    @constructions.reject! { |c| c.dead? }
+  end
+
+  def update_energy_generation(generators)
+    owned_generators = generators.select { |g| g.owner?(self) }
+    owned_generation_capacity = owned_generators.map { |g| g.capacity }.sum
+    @latest_build_capacity = @base_generation_capacity.to_f + owned_generation_capacity
     @energy += @latest_build_capacity
   end
 
-  def build_capacity(generators)
-    owned_generators = generators.select { |g| g.owner?(self) }
-    owned_generation_capacity = owned_generators.map { |g| g.capacity }.sum
-    return @base_generation_capacity.to_f + owned_generation_capacity
+  def update_energy_consumption
+    powered_units = @factories.select(&:producing?)
+    powered_units += @vehicles.select { |u| u.respond_to?(:producing?) && u.producing? }
+    power_drains = Hash[powered_units.map do |powered_unit|
+      [powered_unit.object_id, powered_unit.energy_consumption]
+    end]
+    desired_energy_consumption = power_drains.values.sum
+    if desired_energy_consumption > @energy
+      power_per_unit = @energy / powered_units.length
+      power_drains.transform_values! { power_per_unit }
+    end
+    at_unit_cap =  unit_count < @unit_cap
+    powered_units.each do |powered_unit|
+      # FIXME: do something with unused_build_capacity
+      power_drain = power_drains[powered_unit.object_id]
+      powered_unit.energy_provided = power_drain
+      @energy -= power_drain
+    end
+  end
+
+  def update_constructions
+    @constructions.each do |construction|
+      break if unit_count >= @unit_cap
+      next if !construction.built?
+
+      @constructions.delete(construction)
+      construction.prerender
+      case construction
+      when Factory
+        @factories << construction
+      when Turret
+        @turrets << construction
+      else
+        raise "unknown unit construction complete: '#{construction}'" unless construction.class.ancestors.include?(Vehicle)
+        @vehicles << construction
+      end
+    end
+  end
+
+  def update_turrets_and_projectiles(other_players)
+    enemy_vehicles = other_players.map(&:units).flatten
+    @turrets.each do |turret|
+      projectile = turret.update(enemy_vehicles)
+      @projectiles << projectile unless projectile.nil?
+    end
+    @projectiles.each(&:update)
   end
 end
