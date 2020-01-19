eedde135586782a3d7d0eda83695b5f4999ac0b7
Michael Mokrysz <hi@46b.it>
2020-01-19 08:09:10 +0000

    Add a delay before building construction starts
    This delay makes it less effective to spam up buildings to get in the
    way of turret fire, etc.

--- lib/rts_engine/entities/capabilities/buildable.rb
+++ lib/rts_engine/entities/capabilities/buildable.rb
@@ -3,13 +3,14 @@ require_relative "./killable"
 module Buildable
   include Killable
 
-  attr_reader :built, :cost
+  attr_reader :built, :cost, :delay_before_building
 
-  def initialize_buildable(built: false, max_health:, health: nil, cost:)
+  def initialize_buildable(built: false, max_health:, health: nil, cost:, delay_before_building: 20)
     health = built ? max_health : 0.0 if health.nil?
     initialize_killable(max_health: max_health, health: health)
     @built = built
     @cost = cost
+    @delay_before_building = delay_before_building
   end
 
   def built?
@@ -25,6 +26,10 @@ module Buildable
   end
 
   def repair(*)
+    if @delay_before_building > 0
+      @delay_before_building -= 1
+      return false
+    end
     super
     @built ||= !damaged?
   end
